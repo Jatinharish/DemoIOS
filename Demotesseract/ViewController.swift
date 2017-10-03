57df79c57943057c953b160f89ca15e3b65c49fd
Jatin Kumar <jatin.kumar@isteams-mac-mini-3.local>
2017-10-03 11:23:04 +0530

    Initial Commit

---
+++ Demotesseract/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demotesseract
+//
+//  Created by Jatin Kumar on 03/10/17.
+//  Copyright © 2017 Jatin Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
