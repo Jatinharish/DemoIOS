5c58154342dcbe2cb265763132f9be3de186e8db
Jatin Kumar <jatin.kumar@isteams-Mac-mini-3.local>
2017-11-16 15:29:17 +0530

    first Commit

---
+++ Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  Demotesseract
+//
+//  Created by Jatin Kumar on 03/10/17.
+//  Copyright © 2017 Jatin Kumar. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
